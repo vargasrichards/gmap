eac05bba6f30edb634a70cb164ce057d85b5f378
Alexis Vargas Richards <142454070+vargasrichards@users.noreply.github.com>
2024-07-11 11:58:57 +0100

    modified to use comma separator

--- R/read.map.r
+++ R/read.map.r
@@ -4,6 +4,8 @@
 #' @return Correctly formatted genetic/linkage map
 #' @importFrom utils read.table head
 #' @export
+
+# modified to read csv file
 read.map <- function(file = NULL) {
     # error handling
     if (is.null(file)) {
@@ -11,13 +13,13 @@ read.map <- function(file = NULL) {
     }
 
     # read file containing genetic map
-    init <- read.table(file, sep = "\t", header = FALSE)
+    init <- read.table(file, sep = ",", header = FALSE)
 
     if (ncol(init) == 3) {
-      colnames(init) <- c("marker", "chr", "cM")
+      colnames(init) <- c("marker", "chr", "bp")
     } else {
       head(init)
-      stop("Input table must contain at least 3 columns ('marker','lingroup','cM').")
+      stop("Input table must contain at least 3 columns ('marker','lingroup','bp').")
     }
     return(init)
 }
